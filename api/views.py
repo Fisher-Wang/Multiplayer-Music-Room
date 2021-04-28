ee037a82f0a83f10ce27a830b1e67f2a93d30ed5
Fisher Wang <kingwfsh@163.com>
2021-04-28 19:37:13 +0800

    ListApiView

--- api/views.py
+++ api/views.py
@@ -6,6 +6,6 @@ from .models import Room
 # Create your views here.
 # Endpoints(location on the webserver where you can go to)
 
-class RoomView(generics.CreateAPIView):
+class RoomView(generics.ListAPIView):
     queryset = Room.objects.all()  # what to view
     serializer_class = RoomSerializer  # how to view
