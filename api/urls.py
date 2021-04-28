1f198bde56830a33eae48103e29b5ecb3cb291ed
Fisher Wang <kingwfsh@163.com>
2021-04-28 20:21:17 +0800

    change home to room

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,5 @@ from .views import RoomView
 urlpatterns = [
     # path('home/', main),  # if the url is like '/home/...', send it over to main function in view.py
     # path('', main)  # whatever the url is, send it over to main function in view.py
-    path('home/', RoomView.as_view())
+    path('room', RoomView.as_view())
 ]
