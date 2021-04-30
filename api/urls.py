9bb0418e541e847aff6c1b8e46fadec3dff30738
Fisher Wang <kingwfsh@163.com>
2021-04-30 13:59:49 +0800

    still no idea why 405

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     # path('home/', main),  # if the url is like '/home/...', send it over to main function in view.py
     # path('', main)  # whatever the url is, send it over to main function in view.py
     path('room', RoomView.as_view()),
-    path('create-room', CreateRoomView.as_view())
+    path('create-room', CreateRoomView.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -32,9 +32,9 @@ class CreateRoomView(APIView):
                 room.save(update_fields=['guest_can_pause', 'votes_to_skip'])
                 return Response(RoomSerializer(room).data, status=status.HTTP_200_OK)
             else:
-                room = Room(host=host, guest_can_pause=guest_can_pause, 
+                room = Room(host=host, guest_can_pause=guest_can_pause,
                             votes_to_skip=votes_to_skip)
                 room.save()
                 return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
-            
+
         return Response({'Bad Request': 'Invalid data...'}, status=status.HTTP_400_BAD_REQUEST)
